aa4d5ddba370bb88ccc1e88eabd1b7eb8fdc2f5a
Philip Kessler <philorlando92@yahoo.de>
2016-04-29 20:27:53 +0200

    First commit

---
+++ app/src/main/java/com/pkindustries/labelme/ImageLabelActivity.java
@@ -0,0 +1,20 @@
+package com.pkindustries.labelme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class ImageLabelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_label);
+
+//        ImageView image = (ImageView) findViewById(R.id.main_image);
+//        image.setImageResource(R.drawable.img_dog_002);
+//
+//        Toast.makeText(this, "image loaded", Toast.LENGTH_SHORT).show();
+    }
+}
