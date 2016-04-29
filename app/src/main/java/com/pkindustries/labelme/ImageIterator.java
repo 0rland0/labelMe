633f287f1d04e81f09ad5bfce65142680349510e
Philip Kessler <philorlando92@yahoo.de>
2016-04-29 21:48:55 +0200

    Nine images are now available for labeling

---
+++ app/src/main/java/com/pkindustries/labelme/ImageIterator.java
@@ -0,0 +1,7 @@
+package com.pkindustries.labelme;
+
+/**
+ * Created by Orlando on 4/29/16.
+ */
+public class ImageIterator {
+}
--- app/src/main/java/com/pkindustries/labelme/ImageLabelActivity.java
+++ app/src/main/java/com/pkindustries/labelme/ImageLabelActivity.java
@@ -1,20 +1,44 @@
 package com.pkindustries.labelme;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ImageLabelActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_label);
 
-//        ImageView image = (ImageView) findViewById(R.id.main_image);
-//        image.setImageResource(R.drawable.img_dog_002);
-//
-//        Toast.makeText(this, "image loaded", Toast.LENGTH_SHORT).show();
+
+        final ImageView imageView = (ImageView) findViewById(R.id.main_image);
+
+
+        int imageResourceIds[] = { R.drawable.img_10, R.drawable.img_11, R.drawable.img_12,
+                R.drawable.img_13, R.drawable.img_14, R.drawable.img_15, R.drawable.img_16,
+                R.drawable.img_17, R.drawable.img_18 };
+        final ImageIterator images = new ImageIterator(imageResourceIds);
+
+        Button labelOneButton =  (Button) findViewById(R.id.btn_label_one);
+        final Activity mainView = this;
+
+        labelOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newResourceId = images.next();
+                if(newResourceId != -1) {
+                    imageView.setImageResource(newResourceId);
+                } else {
+                    Toast.makeText(mainView, "There are no more images left!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
